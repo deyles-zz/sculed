639b85876bebafa84ba2cf3c6ce21070e0ec47c8
Dan <daniel.eyles@yahoo.com.au>
2013-07-27 21:21:30 +1000

    added more detailed statistics

--- test_manual/test_server.js
+++ test_manual/test_server.js
@@ -1,25 +1,62 @@
 var client = require('../lib/client.js');
+var uuid   = require('node-uuid');
 
 var counter;
 var queue;
+var hash;
 
 //client.connect('166.78.12.141', 72853, function() { 
 client.connect('127.0.0.1', 72853, function() {
-    counter = client.getCounter('counter', null, function(err, data) {});
-    queue = client.getQueue('queue', null, function(err, data) {});
+    counter = client.getCounter('counter', null, function(err, data) { startCounterTests(); });
+    queue   = client.getQueue('queue', null, function(err, data) { startQueueTests(); });
+    hash    = client.getHashTable('hash', null, function(err, data) { startHashTableTests(); });
 });
 
-setInterval(function() {
-    queue.dequeue(function(err, data) {});
-}, 100);
+function startHashTableTests() {
+    setInterval(function() {
+        if (!hash) {
+            return;
+        }
+        var key = uuid.v4();
+        hash.set(key, (new Date()).getTime());
+    }, 100);
+    setInterval(function() {
+        if (!hash) {
+            return;
+        }    
+        hash.contains(uuid.v4());
+    }, 150);    
+};
 
-setInterval(function() {
-    queue.enqueue('new_' + (new Date()).getTime());
-}, 150);
+function startQueueTests() {
+    setInterval(function() {
+        if (!queue) {
+            return;
+        }
+        queue.dequeue(function(err, data) {});
+    }, 100);
+    setInterval(function() {
+        if (!queue) {
+            return;
+        }    
+        queue.enqueue('new_' + (new Date()).getTime());
+    }, 150);
+};
 
-setInterval(function() {
-    counter.increment(1);
-}, 50);
+function startCounterTests() {
+    setInterval(function() {
+        if (!counter) {
+            return;
+        }    
+        counter.increment(1);
+    }, 50);
+    setInterval(function() {
+        if (!counter) {
+            return;
+        }    
+        counter.count();
+    }, 250);    
+};
 
 setInterval(function() {
     client.getStatistics(function(err, data) {
